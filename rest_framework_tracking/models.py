786f37e39249e19e8b1ba6e709a6ffc8fe6ece95
Mai Soliman <msoliman@Mais-MacBook-Pro.local>
2021-05-07 19:16:10 +0200

    revert remove model

--- rest_framework_tracking/models.py
+++ rest_framework_tracking/models.py
@@ -1,12 +1,4 @@
 from .base_models import BaseAPIRequestLog
 
-'''
-Removing default model to be replaced by model in parent code
-Example:
-class AnalyticsAPIRequestTracking(BaseAPIRequestLog):
-    class Meta:
-        db_table = 'analytics_api_request_tracking'
-        verbose_name = 'Analytics API Request Tracking'
-'''
-#class APIRequestLog(BaseAPIRequestLog):
-#    pass
+class APIRequestLog(BaseAPIRequestLog):
+    pass
